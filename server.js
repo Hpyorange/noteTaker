a9f52f218f0dc4ddfaef67c08564626eed9c3796
Hpyorange <jaspercheng2022@gmail.com>
2022-11-16 15:22:46 -0500

    small change of server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
+
 
 // In many environments (e.g. Heroku), and as a convention || 3001 port
 const PORT = process.env.PORT || 3001;
@@ -8,10 +11,19 @@ const PORT = process.env.PORT || 3001;
 //use public folder
 app.use(express.static('public'));
 
-//Post method
+
+//Post method (required)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//get method '/motes
+app.get('/notes',(req,res)=>{
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
